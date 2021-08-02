d0d9d8733f51bed0efaa5d34876e3778b15c74ee
Erik Westrup <erik.westrup@gmail.com>
2021-08-02 23:47:05 +0200

    Indent tag.rb

--- lib/jekyll-glossary_tooltip/tag.rb
+++ lib/jekyll-glossary_tooltip/tag.rb
@@ -24,28 +24,28 @@ module Jekyll
         HTML
       end
 
-    	private
+      private
 
-    	LOG_TAG = "Glossary Tag:"
+      LOG_TAG = "Glossary Tag:"
 
       def render_tooltip_url(entry)
         # The content of the anchor is set from the CSS class jekyll-glossary-source-link, so that the plugin user can customize the text without touching ruby source.
         entry.key?("url") and entry["url"] ? "<br><a class=\"jekyll-glossary-source-link\" href=\"#{entry["url"]}\"></a>" : ""
       end
 
-    	def lookup_entry(site, term_name)
-    	  entry = read_term_entry_from_config(site, term_name)
-    	  if not (entry.key?('definition') and entry['definition']) # TODO save navigation can also check of the value is non-zero?
+      def lookup_entry(site, term_name)
+    	entry = read_term_entry_from_config(site, term_name)
+    	if not (entry.key?('definition') and entry['definition']) # TODO save navigation can also check of the value is non-zero?
           raise Errors::MissingTermDefinition, "Glossary entry for #{term_name} does not contain a definition!"
         end
         if not entry.key?('url')
           entry['url'] = nil
         end
         entry
-    	end
+      end
 
-    	# Retrieve a term from the glossary via the site.
-    	def read_term_entry_from_config(site, term_name)
+      # Retrieve a term from the glossary via the site.
+      def read_term_entry_from_config(site, term_name)
         raise Errors::NoGlossaryFile, "No data.glossary found" unless site.data['glossary']
 
         entries = site.data['glossary'].select do |entry|
@@ -53,13 +53,13 @@ module Jekyll
         end
 
         if entries.length() == 0
-        	raise Errors::MissingTermEntry, "The term '#{term_name}' was not defined in the glossary"
+          raise Errors::MissingTermEntry, "The term '#{term_name}' was not defined in the glossary"
         elsif entries.length() == 1
-      	return entries[0]
+      	  return entries[0]
         else
-        	raise Errors::MultipleTermEntries, "The term #{term_name} was defined multiple times in the glossary"
-    	  end
+          raise Errors::MultipleTermEntries, "The term #{term_name} was defined multiple times in the glossary"
     	end
+      end
     end
   end
 end

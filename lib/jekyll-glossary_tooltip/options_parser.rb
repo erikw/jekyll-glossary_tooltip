aeadf1613de51d92b11db330db772294f78812e8
Erik Westrup <erik.westrup@gmail.com>
2021-08-06 00:07:54 +0200

    Fix rubocop warnings

--- lib/jekyll-glossary_tooltip/options_parser.rb
+++ lib/jekyll-glossary_tooltip/options_parser.rb
@@ -1,13 +1,16 @@
-# Stripped down & modified version of https://github.com/ayastreb/jekyll-maps/blob/master/lib/jekyll-maps/options_parser.rb
+# frozen_string_literal: true
+
 require "jekyll-glossary_tooltip/errors"
 
 module Jekyll
   module GlossaryTooltip
+    # Stripped down & modified version of
+    # https://github.com/ayastreb/jekyll-maps/blob/master/lib/jekyll-maps/options_parser.rb
     class OptionsParser
-      ARGS_PATTERN = %r!\s*(\w[-_\w]*):\s*(\w[^,\n\r]*)!
-      ARGS_ALLOWED = %w(
+      ARGS_PATTERN = %r{\s*(\w[-_\w]*):\s*(\w[^,\n\r]*)}
+      ARGS_ALLOWED = %w[
         display
-      ).freeze
+      ].freeze
 
       class << self
         def parse(raw_options)
@@ -15,25 +18,20 @@ module Jekyll
             term_query: nil,
             display: nil
           }
-          opt_segments = raw_options.strip.split(',')
-          if opt_segments.length > 0
-            options[:term_query] = opt_segments[0]
-            opt_segments.shift
-          else
-            raise Errors::OptionsNoTermNameInTag
-          end
+          opt_segments = raw_options.strip.split(",")
+          raise Errors::OptionsNoTermNameInTag unless opt_segments.length.positive?
+
+          options[:term_query] = opt_segments[0]
+          opt_segments.shift
+
           opt_segments.each do |opt_segment|
-            if opt_segment =~ ARGS_PATTERN
-              arg_name = $1
-              arg_value = $2.strip
-              if ARGS_ALLOWED.include?(arg_name)
-                options[arg_name.to_sym] = arg_value
-              else
-                raise Errors::OptionsUnknownTagArgument, arg_name
-              end
-            else
-              raise Errors::OptionsBadTagArgumentFormat, options[:term_name]
-            end
+            raise Errors::OptionsBadTagArgumentFormat, options[:term_name] unless opt_segment =~ ARGS_PATTERN
+
+            arg_name = Regexp.last_match(1)
+            arg_value = Regexp.last_match(2)
+            raise Errors::OptionsUnknownTagArgument, arg_name unless ARGS_ALLOWED.include?(arg_name)
+
+            options[arg_name.to_sym] = arg_value
           end
           options
         end
--- spec/jekyll-glossary_tooltip/options_parser_spec.rb
+++ spec/jekyll-glossary_tooltip/options_parser_spec.rb
@@ -3,17 +3,30 @@
 require "jekyll-glossary_tooltip/options_parser"
 require "jekyll-glossary_tooltip/errors"
 
-P= Jekyll::GlossaryTooltip::OptionsParser
+P = Jekyll::GlossaryTooltip::OptionsParser
 E = Jekyll::GlossaryTooltip::Errors
 
 RSpec.describe Jekyll::GlossaryTooltip::OptionsParser do
   context "when providing valid term name" do
+    let(:actual) { P.parse("a_term") }
+    let(:expected) do
+      { term_query: "a_term",
+        display: nil }
+    end
+
     it "parsing works" do
-      actual = P.parse("a_term")
-      expected = {
-        term_query: "a_term",
-        display: nil,
-      }
+      expect(actual).to eq(expected)
+    end
+  end
+
+  context "when providing valid term name and valid arg" do
+    let(:actual) { P.parse("a_term, display: FancyName ") }
+    let(:expected) do
+      { term_query: "a_term",
+        display: "FancyName" }
+    end
+
+    it "parsing works" do
       expect(actual).to eq(expected)
     end
   end
@@ -21,32 +34,18 @@ RSpec.describe Jekyll::GlossaryTooltip::OptionsParser do
   context "when providing no term name query" do
     it "parsing raises error" do
       expect { P.parse("   ") }.to raise_error(E::OptionsNoTermNameInTag)
-
-    end
-  end
-
-  context "when providing valid term name and valid arg" do
-    it "parsing works" do
-      actual = P.parse("a_term, display: FancyName ")
-      expected = {
-        term_query: "a_term",
-        display: "FancyName",
-      }
-      expect(actual).to eq(expected)
     end
   end
 
   context "when providing valid term name and invalid arg" do
     it "parsing raises error" do
       expect { P.parse("a_term, unknownArg: what") }.to raise_error(E::OptionsUnknownTagArgument)
-
     end
   end
 
   context "when providing invalid format" do
     it "parsing raises error" do
       expect { P.parse("a_term, display = FancyName") }.to raise_error(E::OptionsBadTagArgumentFormat)
-
     end
   end
 end

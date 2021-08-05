5ed21a35457d7b77e07406977bd284acc07f14f8
Erik Westrup <erik.westrup@gmail.com>
2021-08-05 23:47:59 +0200

    Fix module require

--- lib/jekyll-glossary_tooltip.rb
+++ lib/jekyll-glossary_tooltip.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require_relative "jekyll-glossary_tooltip/options_parse"
+require_relative "jekyll-glossary_tooltip/options_parser"
 require_relative "jekyll-glossary_tooltip/tag"
 require_relative "jekyll-glossary_tooltip/version"
 
